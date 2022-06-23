014be130399f4f625afa882a4375195e9221d8df
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2022-06-23 15:06:41 +0800

    feat: new example using hex encoding and dart_ecdsa

--- bin/dart_ecdsa_new.dart
+++ bin/dart_ecdsa_new.dart
@@ -11,24 +11,24 @@ void main() {
   // var priv = ec.generatePrivateKey();
   // var pub = priv.publicKey;
 
-  // hex to key pair
+  // hex to key pair (for importing)
   print('---hex to key pair---');
-  var priv = PrivateKey.fromHex(
-      ec, 'd04b6af44bdaca5a3dd10f33572bd917def5697f72823213c8e8da5d07cff7f4');
+  var priv = PrivateKey.fromHex(ec,
+      'd04b6af44bdaca5a3dd10f33572bd917def5697f72823213c8e8da5d07cff7f4'); // sample private key
   var pub = priv.publicKey;
 
-  print('private key: ${priv.toString()}');
-  print('public key: ${pub.toString()}');
+  print('private key: ${priv.toString()}'); // raw hex encoded private key
+  print('public key: ${pub.toString()}'); // raw hex encoded public key
 
   var hashHex = sha256
       .convert(utf8.encode(
-          '{"amount":50000,"login_token":"lt_48gskrqgyzmgvm7j","to":"stuart@coinmode.com","to_type":"email","wallet":"gbpx_main"}'))
+          '{"amount":60000,"login_token":"lt_5dpm7sdc4xk7tbm5","to":"stuart@coinmode.com","to_type":"email","wallet":"gbpx_main"}'))
       .toString(); // hash of message to be signed
 
   var hash = List<int>.generate(hashHex.length ~/ 2,
       (i) => int.parse(hashHex.substring(i * 2, i * 2 + 2), radix: 16));
   var sig = signature(priv, hash);
-  print('signature: ${sig.toDERHex()}');
+  print('signature: ${sig.toDERHex()}'); // signature in hex
 
   var result = verify(pub, hash, sig);
   print('verification result: $result');
